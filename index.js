b8698570e30c70c08a15482aee94225e16ba780b
Wosduch <ivandr2006@gmail.com>
2020-07-19 21:08:57 +0300

    Created basic setup

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const fs = require("fs");
 const token_obj = JSON.parse(fs.readFileSync("token.json"));
 
 const bot = new Telegraf(token_obj.token);
+
+bot.start((ctx) => {
+    ctx.reply("Привет!").then(() => {
+        ctx.reply("Я - админ-бот для этого чата. Пока что я ничего не умею. А еще я был бы очень рад, если бы кто-нибудь предоставил бесплатный сервер для меня.\r\n \r\n По всем предложениям писать сюда: https://github.com/Wosduch/html-mentor-telegram-admin-bot/issues");
+    })
+});
